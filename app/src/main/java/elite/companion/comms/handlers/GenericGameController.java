a70ace45e9211a16f1e22ed6c6690ca7b982e964
Alex Stone (AR) <129783572+AR-AStone@users.noreply.github.com>
2025-08-24 00:50:45 -0700

    removing unused handlers

--- app/src/main/java/elite/companion/comms/handlers/GenericGameController.java
+++ app/src/main/java/elite/companion/comms/handlers/GenericGameController.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GenericGameController implements CommandHandler {
-    private static final Logger log = LoggerFactory.getLogger(DeployLandingGearHandler.class);
+    private static final Logger log = LoggerFactory.getLogger(GenericGameController.class);
     private final VoiceCommandHandler voiceCommandHandler;
     private final String gameBinding;
 

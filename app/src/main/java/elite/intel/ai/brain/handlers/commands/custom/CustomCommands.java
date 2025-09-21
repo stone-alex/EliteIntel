247ecb3a59f31610a1997470fc5b0f35ae231913
Alex Stone <alexstone@sonic.net>
2025-09-20 22:57:06 -0700

    plot route back to carrier, plot route back to home system

--- app/src/main/java/elite/intel/ai/brain/handlers/commands/custom/CustomCommands.java
+++ app/src/main/java/elite/intel/ai/brain/handlers/commands/custom/CustomCommands.java
@@ -46,7 +46,8 @@ public enum CustomCommands {
     EXIT_TO_HUD("display_hud", null, null, ExitToHud.class),
     CLOSE_SYSTEM_MAP("close_local_starsystem_map", null, null, ExitToHud.class),
     SET_HOME_SYSTEM("set_location_as_home_star_system", null, null, SetCurrentStarAsHomeSystem.class),
-    TAKE_ME_HOME("take_us_home", null, null, PlotRouteToHomeHandler.class)
+    TAKE_ME_HOME("take_us_home", null, null, PlotRouteToHomeHandler.class),
+    PLOT_ROUTE_TO_CARRIER("plot_route_to_fleet_carrier", null, null, PlotRouteToMyFleetCarrier.class)
     ;
 
 
---
+++ app/src/main/java/elite/intel/ai/brain/handlers/commands/custom/PlotRouteToMyFleetCarrier.java
@@ -0,0 +1,33 @@
+package elite.intel.ai.brain.handlers.commands.custom;
+
+import com.google.gson.JsonObject;
+import elite.intel.ai.brain.handlers.commands.CommandHandler;
+import elite.intel.ai.hands.GameHandler;
+import elite.intel.gameapi.EventBusManager;
+import elite.intel.gameapi.VoiceProcessEvent;
+import elite.intel.gameapi.journal.events.dto.CarrierDataDto;
+import elite.intel.session.PlayerSession;
+
+public class PlotRouteToMyFleetCarrier extends CustomCommandOperator implements CommandHandler {
+
+    private final GameHandler commandHandler;
+
+    public PlotRouteToMyFleetCarrier(GameHandler commandHandler) {
+        super(commandHandler.getMonitor(), commandHandler.getExecutor());
+        this.commandHandler = commandHandler;
+    }
+
+    @Override public void handle(JsonObject params, String responseText) {
+        PlayerSession playerSession = PlayerSession.getInstance();
+        CarrierDataDto carrierData = playerSession.getCarrierData();
+
+        if (carrierData == null) {
+            EventBusManager.publish(new VoiceProcessEvent("No carrier data found."));
+            return;
+        }
+
+        String carrierLocation = carrierData.getLocation();
+        RoutePlotter plotter = new RoutePlotter(this.commandHandler);
+        plotter.plotRoute(carrierLocation);
+    }
+}

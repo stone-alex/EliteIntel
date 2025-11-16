aaf3a337eb1b12e63f8bdf8301671b3d25b3b4af
alex <alexstone@sonic.net>
2025-11-15 17:28:39 -0800

    search technology brokers and material traders

--- remote-build.sh
+++ remote-build.sh
@@ -1,2 +1,2 @@
-git tag -a v2025.11.14.beta-0110 -m "Linux Support"
-git push origin v2025.11.14.beta-0110
+git tag -a v2025.11.15.beta-0111 -m "Adding Spansh Search"
+git push origin v2025.11.15.beta-0111

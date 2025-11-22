e0d658aca2787fa9b72c15af8c65fb71ab2acf0e
alex <alexstone@sonic.net>
2025-11-22 12:53:18 -0800

    changing SQL schema naming

--- remote-build.sh
+++ remote-build.sh
@@ -1,2 +1,2 @@
-git tag -a v2025.11.21.beta-0121 -m "Spansh Search Updates"
-git push origin v2025.11.21.beta-0121
+git tag -a v2025.11.22.beta-0122 -m "Spansh Search Updates"
+git push origin v2025.11.22.beta-0122

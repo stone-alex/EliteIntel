09bff9755ccedd69d806d2710b892ef66f115c8e
alex <alexstone@sonic.net>
2025-11-28 12:21:35 -0800

    fixing OllamaClient

--- app/src/main/java/elite/intel/ai/brain/ollama/OllamaClient.java
+++ app/src/main/java/elite/intel/ai/brain/ollama/OllamaClient.java
@@ -9,7 +9,7 @@ import java.net.URL;
 
 public class OllamaClient implements Client {
 
-    //public static final String MODEL_TINYLAMA = "tinyllama";
+    public static final String MODEL_TINYLAMA = "tinyllama";
     public static final String MODEL_PFI_MINI = "phi3:mini";
 
     private static final OllamaClient INSTANCE = new OllamaClient();

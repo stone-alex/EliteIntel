57cf8cae5e48aa9aaa039dd9eeafda013c2e568c
alex <alexstone@sonic.net>
2025-12-06 23:23:30 -0800

    Re-calculate and Clear fleet carrier route commands. Some general improvements. Use HIGH_RMS for gating.

--- remote-build.sh
+++ remote-build.sh
@@ -1,2 +1,2 @@
-git tag -a v2025.12.4.beta-0147 -m "Introducing trade route plotting"
-git push origin v2025.12.4.beta-0147
+git tag -a v2025.12.6.beta-0148 -m "Introducing trade route plotting"
+git push origin v2025.12.6.beta-0148

629fe09230d8c73dee5e7e1e0b5fff04943fcb17
alex <alexstone@sonic.net>
2025-12-10 11:59:59 -0800

    Fixing SystemSession bug (blank keys for off-line TTS)

--- remote-build.sh
+++ remote-build.sh
@@ -1,2 +1,2 @@
-git tag -a v2025.12.9.beta-0157 -m "Fixing pirate missions summary"
-git push origin v2025.12.9.beta-0157
+git tag -a v2025.12.10.beta-0158 -m "Fixing SystemSession bug (blank keys for off-line TTS)"
+git push origin v2025.12.10.beta-0158

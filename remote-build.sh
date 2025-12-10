c4b4e965ae1c8d94d79b26b3751b772ac2ddf407
alex <alexstone@sonic.net>
2025-12-09 19:05:44 -0800

    Fixing pirate mission summary.

--- remote-build.sh
+++ remote-build.sh
@@ -1,2 +1,2 @@
-git tag -a v2025.12.9.beta-0155 -m "Encrypting keys"
-git push origin v2025.12.9.beta-0155
+git tag -a v2025.12.9.beta-0156 -m "Fixing pirate missions summary"
+git push origin v2025.12.9.beta-0156

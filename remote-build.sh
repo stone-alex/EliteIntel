7ec4728d15f0e12a1d44f0ab3ef4508498469708
alex <alexstone@sonic.net>
2025-12-19 19:20:56 -0800

    clean up.

--- remote-build.sh
+++ remote-build.sh
@@ -1,2 +1,2 @@
-git tag -a v2025.12.17.beta-0161 -m "Target subsystem"
-git push origin v2025.12.17.beta-0161
+git tag -a v2025.12.19.beta-0162 -m "Target subsystem"
+git push origin v2025.12.19.beta-0162

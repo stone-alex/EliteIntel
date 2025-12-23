ea6bb818cd1999e921eb406df5dca2ffa468b015
alex <alexstone@sonic.net>
2025-12-22 20:07:07 -0800

    version from resources.

--- remote-build.sh
+++ remote-build.sh
@@ -1,2 +1,2 @@
-git tag -a "$(cat app/src/resources/version.txt)" -m "Target subsystem"
-git push origin "$(cat app/src/resources/version.txt)"
+git tag -a "$(cat app/src/main/resources/version.txt)" -m "Target subsystem"
+git push origin "$(cat app/src/main/resources/version.txt)"

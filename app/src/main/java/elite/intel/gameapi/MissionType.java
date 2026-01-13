f75e9d4b9ad17fc8dec1203bf4863d5a74b092e7
hrHVN <nesheim123@hotmail.com>
2026-01-13 21:49:02 +0100

    adding passanger missions

--- app/src/main/java/elite/intel/gameapi/MissionType.java
+++ app/src/main/java/elite/intel/gameapi/MissionType.java
@@ -25,25 +25,36 @@ public enum MissionType {
     MISSION_DELIVERY("Mission_Delivery"),
 
     //  Passenger
-    //PASSENGER("Passenger"),
-
-    // Rescue
+    MISSION_PASSENGERBULK("Mission_PassengerBulk"),
+    MISSION_PASSENGERVIP("Mission_PassengerVIP"),
+    MISSION_SIGHTSEEING("Mission_Sightseeing"),
     MISSION_RESCUE_ELECTIONS("Mission_Rescue_Elections"),
     MISSION_RESCUE_ELECTIONS_NAME("Mission_Rescue_Elections_name"),
+    MISSION_LONGDISTANCEEXPEDITION_EXPLORER_BOOM("Mission_LongDistanceExpedition_Explorer_Boom"),
+    MISSION_PASSENGERBULK_AIDWORKER_ARRIVING("Mission_PassengerBulk_AIDWORKER_ARRIVING"),
+    MISSION_PASSENGERVIP_TERRORIST_CIVILWAR("Mission_PassengerVIP_Terrorist_CIVILWAR"),
+    MISSION_SIGHTSEEING_CRIMINAL_CIVILWAR("Mission_Sightseeing_Criminal_CIVILWAR"),
+    MISSION_SIGHTSEEING_SCIENTIST_CIVILWAR("Mission_Sightseeing_Scientist_CIVILWAR"),
+    MISSION_SIGHTSEEING_TERRORIST_CIVILWAR("Mission_Sightseeing_Terrorist_CIVILWAR"),
+    MISSION_SIGHTSEEING_TOURIST_BOOM("Mission_Sightseeing_Tourist_BOOM"),
+    MISSION_SIGHTSEEING_WHISTLEBLOWER_ELECTION("Mission_Sightseeing_Whistleblower_ELECTION"),
+    MISSION_PASSENGERBULK_PRISONEROFWAR_LEAVING("Mission_PassengerBulk_PRISONEROFWAR_LEAVING"),
 
     // Alturism
     MISSION_ALTRUISM("Mission_Altruism"),
-    MISSION_ALTRUISMCREDITS_OUTBREAK("Mission_AltruismCredits_Outbreak"),
     MISSION_ALTRUISMCREDITS("Mission_AltruismCredits"),
     MISSION_ALTRUISMCREDITS_NAME("Mission_AltruismCredits_name"),
+    MISSION_ALTRUISMCREDITS_OUTBREAK("Mission_AltruismCredits_Outbreak"),
     MISSION_ALTRUISMCREDITS_OUTBREAK_NAME("Mission_AltruismCredits_Outbreak_name"),
 
     // Salvage
-    MISSION_SALVAGE_ILLEGAL("MISSION_Salvage_Illegal"),
+    MISSION_SALVAGE_ILLEGAL("Mission_Salvage_Illegal"),
 
     // Collecting
     MISSION_COLLECT_INDUSTRIAL("Mission_Collect_Industrial"),
 
+    // Mining
+    MISSION_MINING("Mission_Mining"),
     // OnFoot,
     MISSION_ONFOOT_ONSLAUGHT_OFFLINE_002("Mission_OnFoot_Onslaught_Offline_002"),
     MISSION_ONFOOT_ONSLAUGHT_OFFLINE_002_NAME("Mission_OnFoot_Onslaught_Offline_002_name");
@@ -58,4 +69,4 @@ public enum MissionType {
         return this.missionType;
     }
 
-}
+    }

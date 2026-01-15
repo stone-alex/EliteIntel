aeae8bd9883f113193f13f8f75d81042352e9b03
alex <alexstone@sonic.net>
2026-01-14 19:37:41 -0800

    remark

--- app/src/main/java/elite/intel/ai/brain/handlers/commands/Commands.java
+++ app/src/main/java/elite/intel/ai/brain/handlers/commands/Commands.java
@@ -29,14 +29,16 @@ public enum Commands {
     FIND_ENCODED_MATERIAL_TRADER("find_encoded_material_trader", null, "key", FindEncodedMaterialTraderHandler.class),
     FIND_MANUFACTURED_MATERIAL_TRADER("find_manufactured_material_trader", null, "key", FindManufacturedMaterialTraderHandler.class),
 
+    /// >>> Need to make sure LLM does not confuse the mission commands
     FIND_HUNTING_GROUNDS("find_hunting_grounds_for_pirate_massacre_missions", null, "key", LocatePirateHuntingGrounds.class),
     RECON_TARGET_SYSTEM("navigate_plot_reconnaissance_route_to_hunting_grounds_target_star_system", null, null, ReconPirateMissionTargetSystemHandler.class),
     RECON_PROVIDER_SYSTEM("navigate_plot_reconnaissance_route_to_mission_provider_system", null, null, ReconMissionProviderSystemHandler.class),
+
     NAVIGATE_TO_PIRATE_MISSION_TARGET_SYSTEM("plot_route_to_pirate_massacre_mission_target_system",null, null, NavigateToPirateMassacreMissionTargetHandler.class),
-   // NAVIGATE_TO_MISSION_DESTINATION("plot_route_to_mission_destination",null,null, PlotRouteToNextMissionDestination.class),
-    NAVIGATE_TO_NEXT_MISSION_DESTINATION("plot_route_to_next_mission_destination",null,null, PlotRouteToNextMissionDestination.class),
+    NAVIGATE_TO_NEXT_MISSION("plot_route_to_next_mission",null,null, PlotRouteToNextMissionDestination.class),
 
     NAVIGATE_TO_MISSION_BATTLE_GROUND("navigate_to_mission_battle_ground", null, null, PloteRouteToPirateMissionArena.class),
+    /// <<<
 
     MONETIZE_ROUTE("monetize_route", null, null, MonetizeRouteHandler.class),
 

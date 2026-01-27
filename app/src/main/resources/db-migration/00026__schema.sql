dd8259bc57672beed24a73b393670de9286cf428
alex <alexstone@sonic.net>
2026-01-26 18:24:11 -0800

    bindings for on-foot map/recall/dispmiss ship

--- app/src/main/resources/db-migration/00026__schema.sql
+++ app/src/main/resources/db-migration/00026__schema.sql
@@ -1,3 +1,3 @@
 alter table player add column localLlmAddress text default 'http://localhost:11434';
-alter table game_session add column localLlmCommandModel text default 'qwen2.5:32b';
-alter table game_session add column localLlmQueryModel text default 'qwen2.5:32b';
+alter table game_session add column localLlmCommandModel text default 'qwen2.5:14b';
+alter table game_session add column localLlmQueryModel text default 'qwen2.5:14b';

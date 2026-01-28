002e5932ddeecccf3f9160f29af5bd5ade029b61
hrHVN <nesheim123@hotmail.com>
2026-01-28 15:00:33 +0100

    added subscribers to Accepted and Abandoned missions + minor cleanup

--- app/src/main/java/elite/intel/gameapi/journal/subscribers/MissionAbandonedSubscriber.java
+++ app/src/main/java/elite/intel/gameapi/journal/subscribers/MissionAbandonedSubscriber.java
@@ -3,43 +3,25 @@ package elite.intel.gameapi.journal.subscribers;
 import com.google.common.eventbus.Subscribe;
 import elite.intel.db.managers.MissionManager;
 import elite.intel.gameapi.EventBusManager;
-import elite.intel.gameapi.MissionType;
 import elite.intel.gameapi.SensorDataEvent;
-import elite.intel.gameapi.journal.events.MissionCompletedEvent;
+import elite.intel.gameapi.journal.events.MissionAbandonedEvent;
 import elite.intel.gameapi.journal.events.dto.MissionDto;
-import elite.intel.session.PlayerSession;
-
-import static elite.intel.gameapi.MissionType.MISSION_PIRATE_MASSACRE;
-import static elite.intel.gameapi.MissionType.MISSION_PIRATE_MASSACRE_WING;
-import static elite.intel.util.StringUtls.removeNameEnding;
 
 @SuppressWarnings("unused")
 public class MissionAbandonedSubscriber {
+    private MissionManager missionManager = MissionManager.getInstance();
 
     @Subscribe
-    public void onMissionCompletedEvent(MissionCompletedEvent event) {
-        PlayerSession playerSession = PlayerSession.getInstance();
-        MissionDto mission = playerSession.getMission(event.getMissionID());
-        MissionType missionType = MissionManager.getInstance()
-                .getMissionType(removeNameEnding(event.getName()));
+    public void onMissionAbandonedEvent(MissionAbandonedEvent event) {
+        MissionDto mission = missionManager.getMission(event.getMissionID());
 
         if (mission == null) {
             return; // no mission in session storage. just exit.
         }
-        if (MISSION_PIRATE_MASSACRE.equals(missionType) || MISSION_PIRATE_MASSACRE_WING.equals(missionType)) {
-            playerSession.removeMission(event.getMissionID());
-            String targetFaction = event.getTargetFaction();
-            EventBusManager.publish(new SensorDataEvent("Notify: Mission against Faction \"" + targetFaction + "\" Completed: " + event, "Notify user of a successful mission completion, provide detailed summary from the data received."));
-        }
         else {
-            /*
-                TODO: If user is on a mission with staging.
-                 i.e. a return address when completed, add the return address/details to the database
-                 and delete when the "Missions" event is fired
-             */
-            playerSession.removeMission(event.getMissionID());
-            String missionDetails = event.getLocalisedName();
-            EventBusManager.publish(new SensorDataEvent("Notify: Mission \"" + missionDetails + "\" Completed: " + event, "Notify user of a successful mission completion, provide detailed summary from the data received."));
+            missionManager.remove(event.getMissionID());
+            String missionDetails = mission.getMissionDescription();
+            EventBusManager.publish(new SensorDataEvent("Notify: Mission \"" + missionDetails + "\" Abandoned: " + mission, "Notify user of mission abandonment, provide short summary from the data received."));
         }
 
     }

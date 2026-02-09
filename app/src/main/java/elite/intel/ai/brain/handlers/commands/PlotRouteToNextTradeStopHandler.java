98fc1ae0816ea806a8a59cd69006db544273c557
alex <alexstone@sonic.net>
2026-02-08 19:21:53 -0800

    small TTS Improvements

--- app/src/main/java/elite/intel/ai/brain/handlers/commands/PlotRouteToNextTradeStopHandler.java
+++ app/src/main/java/elite/intel/ai/brain/handlers/commands/PlotRouteToNextTradeStopHandler.java
@@ -3,10 +3,12 @@ package elite.intel.ai.brain.handlers.commands;
 import com.google.gson.JsonObject;
 import elite.intel.ai.hands.GameController;
 import elite.intel.ai.mouth.subscribers.events.MissionCriticalAnnouncementEvent;
+import elite.intel.db.managers.LocationManager;
 import elite.intel.db.managers.ReminderManager;
 import elite.intel.db.managers.TradeRouteManager;
 import elite.intel.gameapi.EventBusManager;
 import elite.intel.gameapi.gamestate.dtos.GameEvents;
+import elite.intel.gameapi.journal.events.dto.LocationDto;
 import elite.intel.search.spansh.station.marketstation.TradeStopDto;
 import elite.intel.search.spansh.traderoute.TradeCommodity;
 import elite.intel.session.PlayerSession;
@@ -16,6 +18,9 @@ import java.util.List;
 public class PlotRouteToNextTradeStopHandler extends CommandOperator implements CommandHandler {
 
     private final PlayerSession playerSession = PlayerSession.getInstance();
+    private final TradeRouteManager tradeRouteManager = TradeRouteManager.getInstance();
+    private final ReminderManager reminderManager = ReminderManager.getInstance();
+    private final LocationManager locationManager = LocationManager.getInstance();
     private final GameController gameController;
 
     public PlotRouteToNextTradeStopHandler(GameController controller) {
@@ -25,9 +30,7 @@ public class PlotRouteToNextTradeStopHandler extends CommandOperator implements 
 
     @Override public void handle(String action, JsonObject params, String responseText) {
         final RoutePlotter routePlotter = new RoutePlotter(gameController);
-        final TradeRouteManager tradeRouteManager = TradeRouteManager.getInstance();
-        final ReminderManager reminderManager = ReminderManager.getInstance();
-
+        final LocationDto location = locationManager.findByLocationData(playerSession.getLocationData());
         if (!tradeRouteManager.hasRoute()) {
             EventBusManager.publish(new MissionCriticalAnnouncementEvent("No trade route found."));
             return;
@@ -37,7 +40,7 @@ public class PlotRouteToNextTradeStopHandler extends CommandOperator implements 
         boolean cargoLoaded = shipCargo.getCount() > 0;
 
         TradeRouteManager.TradeRouteLegTuple<Integer, TradeStopDto> nextStop = tradeRouteManager.getNextStop();
-        if(nextStop == null){
+        if (nextStop == null) {
             EventBusManager.publish(new MissionCriticalAnnouncementEvent("No more stops to visit."));
             return;
         }
@@ -47,16 +50,41 @@ public class PlotRouteToNextTradeStopHandler extends CommandOperator implements 
         if (!cargoLoaded) {
             String sourceSystem = nextStop.getTradeStopDto().getSourceSystem();
             String sourceStation = nextStop.getTradeStopDto().getSourceStation();
-            sb.append("We are heading to ").append(sourceSystem).append(", ").append(sourceStation).append(" there we will pick up ");
+
+            boolean notInSourceSystem = !location.getStarName().equalsIgnoreCase(sourceSystem);
+            boolean notAtTheSourceStation = !location.getStationName().equalsIgnoreCase(sourceStation);
+
+            if (notInSourceSystem) {
+                sb.append(" Travel to ").append(sourceSystem).append(", ");
+                sb.append( " visit " ).append(sourceStation).append(" and pick up ");
+            } else if(notAtTheSourceStation) {
+                sb.append(" In this star system visit ");
+                sb.append(sourceStation).append(" and pick up ");
+            } else {
+                sb.append(" At this station ");
+            }
             commodities.forEach(commodity -> sb.append(commodity.getName()).append(", "));
+
+            if (notInSourceSystem) {
+                routePlotter.plotRoute(sourceSystem);
+            }
             EventBusManager.publish(new MissionCriticalAnnouncementEvent(sb.toString()));
-            routePlotter.plotRoute(sourceSystem);
         } else {
             String destinationSystem = nextStop.getTradeStopDto().getDestinationSystem();
             String destinationStation = nextStop.getTradeStopDto().getDestinationStation();
-            sb.append("We are heading to ").append(destinationSystem).append(", ").append(destinationStation).append(" to sell the freight.");
+
+            boolean notInDestinationSystem = !location.getStarName().equalsIgnoreCase(destinationSystem);
+            boolean notAtTheDestinationStation = !location.getStationName().equalsIgnoreCase(destinationStation);
+
+            if (notInDestinationSystem) {
+                sb.append(" Travel to ").append(destinationSystem).append(", ").append(destinationStation).append(" to sell the freight.");
+                routePlotter.plotRoute(destinationSystem);
+            } else if( notAtTheDestinationStation) {
+                sb.append(" Head to ").append(destinationStation).append(" to sell the freight.");
+            } else {
+                sb.append(" Sell freigh here. ");
+            }
             EventBusManager.publish(new MissionCriticalAnnouncementEvent(sb.toString()));
-            routePlotter.plotRoute(destinationSystem);
         }
 
         reminderManager.setReminder(sb.toString());

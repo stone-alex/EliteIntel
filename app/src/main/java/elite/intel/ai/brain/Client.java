12ba1a2eb42215e1f64c69157de3ea9783250ba4
stone-alex <alexstone@sonic.net>
2026-04-24 18:51:40 -0700

    - refactored clients to not use deprecated http client api.

--- app/src/main/java/elite/intel/ai/brain/Client.java
+++ app/src/main/java/elite/intel/ai/brain/Client.java
@@ -3,8 +3,14 @@ package elite.intel.ai.brain;
 import com.google.gson.JsonObject;
 
 public interface Client {
+
+    /// Local LLMs
     JsonObject createPrompt(int model, float temp);
+
+    /// Cloud LLMs
     JsonObject createPrompt(String model, float temp);
+
     JsonObject createErrorResponse(String message);
+
     JsonObject sendJsonRequest(String request);
 }

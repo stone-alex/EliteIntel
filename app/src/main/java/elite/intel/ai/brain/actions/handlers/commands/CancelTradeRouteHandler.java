3055e0327e438124b2f47313f38b0d50bb263795
stone-alex <alexstone@sonic.net>
2026-06-07 15:06:38 -0700

    fixed about / abort. Fixed cancel trade route.

---
+++ app/src/main/java/elite/intel/ai/brain/actions/handlers/commands/CancelTradeRouteHandler.java
@@ -0,0 +1,17 @@
+package elite.intel.ai.brain.actions.handlers.commands;
+
+import com.google.gson.JsonObject;
+import elite.intel.ai.mouth.subscribers.events.MissionCriticalAnnouncementEvent;
+import elite.intel.db.managers.TradeRouteManager;
+import elite.intel.gameapi.EventBusManager;
+
+public class CancelTradeRouteHandler implements CommandHandler {
+
+    private final TradeRouteManager tradeRouteManager = TradeRouteManager.getInstance();
+
+    @Override
+    public void handle(String action, JsonObject params, String responseText) {
+        tradeRouteManager.clear();
+        EventBusManager.publish(new MissionCriticalAnnouncementEvent("Trade route cancelled"));
+    }
+}

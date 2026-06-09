b052f2e0273634a767b7879f28defac4104f55f7
stone-alex <alexstone@sonic.net>
2026-06-09 10:44:24 -0700

    adding displayName and description to GoogleVoices enum

--- app/src/main/java/elite/intel/ai/mouth/GoogleVoices.java
+++ app/src/main/java/elite/intel/ai/mouth/GoogleVoices.java
@@ -3,44 +3,48 @@ package elite.intel.ai.mouth;
 /**
  * If you implement another TTS, map these voices in your class to the
  * voices available in your TTS provider.
- *
  */
 public enum GoogleVoices {
-    //BETTY("Betty", 1.2, false),         // en-GB-Chirp3-HD-Aoede <-- change me!
-    //CHARLES("Charles", 1.2, true),      // en-GB-Chirp3-HD-Algenib <-- change me!
-    //KAREN("Karen", 1.2, false),         // en-GB-Neural2-A <-- change me!
 
-    MARY("Mary", 1.3, false),           // en-US-Chirp3-HD-Zephyr
-    ANNA("Anna", 1.1, false),           // en-GB-Chirp-HD-F
-    EMMA("Emma", 1.2, false),           // en-US-Chirp3-HD-Despina
-    JAKE("Jake", 1.2, false),           // en-US-Chirp3-HD-Iapetus
-    JAMES("James", 1.2, true),          // en-AU-Chirp3-HD-Algieba
-    JENNIFER("Jennifer", 1.2, false),   // en-US-Chirp3-HD-Sulafat
-    JOSEPH("Joseph", 1.1, true),        // en-US-Chirp3-HD-Sadachbia
-    MICHAEL("Michael", 1.2, true),      // en-US-Chirp3-HD-Charon
-    OLIVIA("Olivia", 1.2, false),       // en-GB-Chirp3-HD-Aoede
-    RACHEL("Rachel", 1.2, false),       // en-US-Chirp3-HD-Zephyr
-    STEVE("Steve", 1.2, true),          // en-US-Chirp3-HD-Algenib
+    MARY("Mary", 1.3, false, "American female"),   // en-US-Chirp3-HD-Zephyr
+    ANNA("Anna", 1.1, false, "British female"),    // en-GB-Chirp-HD-F
+    EMMA("Emma", 1.2, false, "American female"),   // en-US-Chirp3-HD-Despina
+    JAKE("Jake", 1.2, false, "American female"),   // en-US-Chirp3-HD-Iapetus
+    JAMES("James", 1.2, true, "Australian male"),   // en-AU-Chirp3-HD-Algieba
+    JENNIFER("Jennifer", 1.2, false, "American female"), // en-US-Chirp3-HD-Sulafat
+    JOSEPH("Joseph", 1.1, true, "American male"),     // en-US-Chirp3-HD-Sadachbia
+    MICHAEL("Michael", 1.2, true, "American male"),    // en-US-Chirp3-HD-Charon
+    OLIVIA("Olivia", 1.2, false, "British female"),    // en-GB-Chirp3-HD-Aoede
+    RACHEL("Rachel", 1.2, false, "American female"),   // en-US-Chirp3-HD-Zephyr
+    STEVE("Steve", 1.2, true, "American male"),     // en-US-Chirp3-HD-Algenib
     ;
 
-    // Default voice settings
-    private String name = "James";
-    private double speechRate = 1.2;
-    private boolean isMale = true;
+    private final String name;
+    private final double speechRate;
+    private final boolean isMale;
+    private final String description;
 
-
-    GoogleVoices(String name, double speechRate, boolean gender) {
+    GoogleVoices(String name, double speechRate, boolean isMale, String description) {
         this.name = name;
         this.speechRate = speechRate;
-        this.isMale = gender;
-    }
-
-    public double getSpeechRate() {
-        return speechRate;
+        this.isMale = isMale;
+        this.description = description;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getDisplayName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getSpeechRate() {
+        return speechRate;
     }
 
     public boolean isMale() {
